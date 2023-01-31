609396c2bceda795fc939d285ea7eda3298fd1ac
kokhodze <jelo.kokhodze@gmail.com>
2023-01-31 15:56:35 +0400

    fix: update build

--- src/components/Icon/Icons/components/MoveArrowLeft.tsx
+++ src/components/Icon/Icons/components/MoveArrowLeft.tsx
@@ -1,28 +1,16 @@
 /* eslint-disable */
 /* tslint:disable */
-import * as React from "react";
+import * as React from 'react';
 export interface MoveArrowLeftProps extends React.SVGAttributes<SVGElement> {
-  size?: string | number;
+size?: string | number;
 }
-const MoveArrowLeft: React.FC<MoveArrowLeftProps> = ({ size, ...props }) => (
-  <svg
-    width={size || "6"}
-    height={size || "10"}
-    viewBox="0 0 6 10"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-    {...props}
-  >
-    <path
-      d="M4.5 8.75L0.75 5L4.5 1.25"
-      stroke="white"
-      stroke-width="1.5"
-      stroke-linecap="round"
-      stroke-linejoin="round"
-    />
+const MoveArrowLeft: React.FC<MoveArrowLeftProps> = ({size, ...props}) => (
+  <svg viewBox="0 0 20 20" fill="currentColor" width={ size || "20" } height={ size || "20" } {...props}>
+    <path d="M17.8921 10.071C17.8921 10.4853 17.5563 10.821 17.1421 10.821L4.81065 10.821L9.43897 15.4494C9.73186 15.7423 9.73186 16.2171 9.43897 16.51C9.14608 16.8029 8.6712 16.8029 8.37831 16.51L2.46966 10.6014C2.17677 10.3085 2.17677 9.83361 2.46966 9.54071L8.37831 3.63207C8.67121 3.33917 9.14608 3.33917 9.43897 3.63207C9.73187 3.92496 9.73187 4.39983 9.43897 4.69273L4.81065 9.32104L17.1421 9.32104C17.5563 9.32104 17.8921 9.65683 17.8921 10.071Z"
+      fill="currentColor" fillRule="evenodd" clipRule="evenodd" />
   </svg>
 );
-MoveArrowLeft.displayName = "MoveArrowLeft";
+MoveArrowLeft.displayName = 'MoveArrowLeft';
 export default MoveArrowLeft;
 /* tslint:enable */
 /* eslint-enable */
