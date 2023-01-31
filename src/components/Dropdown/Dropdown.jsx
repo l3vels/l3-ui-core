1b6990fda2f0ca4e4fa1433009ed4b365592b3a1
kokhodze <jelo.kokhodze@gmail.com>
2023-01-31 19:52:28 +0400

    fix: update kinds

--- src/components/Dropdown/Dropdown.jsx
+++ src/components/Dropdown/Dropdown.jsx
@@ -16,7 +16,7 @@ import MultiValueContainer from "./components/MultiValueContainer/MultiValueCont
 import { ADD_AUTO_HEIGHT_COMPONENTS, defaultCustomStyles, DROPDOWN_ID } from "./DropdownConstants";
 import generateBaseStyles, { customTheme } from "./Dropdown.styles";
 import Control from "./components/Control/Control";
-import { DROPDOWN_TAG_COLORS, MENU_WRAPPER_CLASS_NAME } from "./dropdown-constants";
+import { DROPDOWN_TAG_COLORS, MENU_WRAPPER_CLASS_NAME, DROPDOWN_KINDS } from "./dropdown-constants";
 import "./Dropdown.scss";
 
 const Dropdown = ({
@@ -67,7 +67,8 @@ const Dropdown = ({
   insideOverflowContainer,
   insideOverflowWithTransformContainer,
   ref,
-  tooltipContent
+  tooltipContent,
+  kind
 }) => {
   const controlRef = useRef();
   const overrideDefaultValue = useMemo(() => {
@@ -251,9 +252,20 @@ const Dropdown = ({
     [insideOverflowContainer, id, insideOverflowWithTransformContainer]
   );
 
+  const kind_wrapper_className =
+    kind === DROPDOWN_KINDS.PRIMARY
+      ? "primary__wrapper"
+      : kind === DROPDOWN_KINDS.SECONDARY
+      ? "secondary__wrapper"
+      : kind === DROPDOWN_KINDS.TERTIARY
+      ? "tertiary__wrapper"
+      : "";
+
+  console.log("🚀 ~ file: Dropdown.jsx:256 ~ kind_wrapper_className", kind_wrapper_className);
+
   return (
     <DropDownComponent
-      className={cx("dropdown-wrapper", className)}
+      className={cx("dropdown-wrapper", className, kind_wrapper_className)}
       selectProps={customProps}
       components={{
         DropdownIndicator,
@@ -310,6 +322,7 @@ const Dropdown = ({
 
 Dropdown.size = SIZES;
 Dropdown.tagColors = DROPDOWN_TAG_COLORS;
+Dropdown.kind = DROPDOWN_KINDS;
 
 Dropdown.defaultProps = {
   className: "",
@@ -337,7 +350,8 @@ Dropdown.defaultProps = {
   withMandatoryDefaultOptions: false,
   insideOverflowContainer: false,
   insideOverflowWithTransformContainer: false,
-  tooltipContent: ""
+  tooltipContent: "",
+  kind: DROPDOWN_KINDS.PRIMARY
 };
 
 Dropdown.propTypes = {
@@ -547,7 +561,9 @@ Dropdown.propTypes = {
   /**
    * When content is passed, the dropdown will include a tooltip on the dropdown's value.
    */
-  tooltipContent: PropTypes.string
+  tooltipContent: PropTypes.string,
+
+  kind: PropTypes.DROPDOWN_KINDS
 };
 
 export default Dropdown;
