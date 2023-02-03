42688e6ab8acb086697920a6fc313d7e0066dae3
kokhodze <jelo.kokhodze@gmail.com>
2023-02-03 13:07:31 +0400

    fix: colors

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -57,7 +57,7 @@ addParameters({
     canvas: { title: "Sandbox" }
   },
   themes: {
-    default: "Black",
+    default: "Light",
     list: [
       { name: "Dark", class: "dark-app-theme", color: "#1C1F3B" },
       { name: "Black", class: "black-app-theme", color: "#111111" },
