e1e7c49cfc135c732fc0af8b6c44c6ce619dcc0a
DavitBakhutashvili <davit.bakhutashvili1@gmail.com>
2023-03-01 21:52:33 +0400

    fix: added avatar list

--- src/components/Avatar/__stories__/assets/index.js
+++ src/components/Avatar/__stories__/assets/index.js
@@ -11,33 +11,33 @@ import avatar1 from "./avatar1.png";
 import avatar2 from "./avatar2.png";
 import avatar3 from "./avatar3.png";
 import avatar4 from "./avatar4.png";
-import apes1 from "./apes1.jpg";
-import apes2 from "./apes2.jpg";
-import apes3 from "./apes3.jpg";
-import apes4 from "./apes4.jpg";
-import apes5 from "./apes5.jpg";
-import apes6 from "./apes6.jpg";
-import azuki1 from "./azuki1.jpg";
-import azuki2 from "./azuki2.jpg";
-import azuki3 from "./azuki3.jpg";
-import azuki4 from "./azuki4.jpg";
-import clonex1 from "./clonex1.jpg";
-import clonex2 from "./clonex2.jpg";
-import clonex3 from "./clonex3.jpg";
-import clonex4 from "./clonex4.jpg";
-import clonex5 from "./clonex5.jpg";
-import clonex6 from "./clonex6.jpg";
-import clonex7 from "./clonex7.jpg";
-import clonex8 from "./clonex8.jpg";
-import clonex9 from "./clonex9.jpg";
-import clonex10 from "./clonex10.jpg";
-import clonex11 from "./clonex11.jpg";
-import moonbird1 from "./moonbird1.jpg";
-import moonbird2 from "./moonbird2.jpg";
-import moonbird4 from "./moonbird4.jpg";
-import moonbird5 from "./moonbird5.jpg";
-import moonbird6 from "./moonbird6.jpg";
-import moonbird7 from "./moonbird7.jpg";
+import monkey_1 from "./monkey_1.jpg";
+import monkey_2 from "./monkey_2.jpg";
+import monkey_3 from "./monkey_3.jpg";
+import monkey_4 from "./monkey_4.jpg";
+import monkey_5 from "./monkey_5.jpg";
+import monkey_6 from "./monkey_6.jpg";
+import azuki_1 from "./azuki_1.jpg";
+import azuki_2 from "./azuki_2.jpg";
+import azuki_3 from "./azuki_3.jpg";
+import azuki_4 from "./azuki_4.jpg";
+import clone_1 from "./clone_1.jpg";
+import clone_2 from "./clone_2.jpg";
+import clone_3 from "./clone_3.jpg";
+import clone_4 from "./clone_4.jpg";
+import clone_5 from "./clone_5.jpg";
+import clone_6 from "./clone_6.jpg";
+import clone_7 from "./clone_7.jpg";
+import clone_8 from "./clone_8.jpg";
+import clone_9 from "./clone_9.jpg";
+import clone_10 from "./clone_10.jpg";
+import clone_11 from "./clone_11.jpg";
+import bird_1 from "./bird_1.jpg";
+import bird_2 from "./bird_2.jpg";
+import bird_3 from "./bird_3.jpg";
+import bird_4 from "./bird_4.jpg";
+import bird_5 from "./bird_5.jpg";
+import bird_6 from "./bird_6.jpg";
 
 export {
   home,
@@ -53,31 +53,181 @@ export {
   avatar2,
   avatar3,
   avatar4,
-  apes1,
-  apes2,
-  apes3,
-  apes4,
-  apes5,
-  apes6,
-  azuki1,
-  azuki2,
-  azuki3,
-  azuki4,
-  clonex1,
-  clonex2,
-  clonex3,
-  clonex4,
-  clonex5,
-  clonex6,
-  clonex7,
-  clonex8,
-  clonex9,
-  clonex10,
-  clonex11,
-  moonbird1,
-  moonbird2,
-  moonbird4,
-  moonbird5,
-  moonbird6,
-  moonbird7
+  monkey_1,
+  monkey_2,
+  monkey_3,
+  monkey_4,
+  monkey_5,
+  monkey_6,
+  azuki_1,
+  azuki_2,
+  azuki_3,
+  azuki_4,
+  clone_1,
+  clone_2,
+  clone_3,
+  clone_4,
+  clone_5,
+  clone_6,
+  clone_7,
+  clone_8,
+  clone_9,
+  clone_10,
+  clone_11,
+  bird_1,
+  bird_2,
+  bird_3,
+  bird_4,
+  bird_5,
+  bird_6
 };
+
+export const Avatars = [
+  {
+    name: "monkey_1",
+    image: monkey_1
+  },
+  {
+    name: "monkey_2",
+    image: monkey_2
+  },
+  {
+    name: "monkey_3",
+    image: monkey_3
+  },
+  {
+    name: "monkey_4",
+    image: monkey_4
+  },
+  {
+    name: "monkey_5",
+    image: monkey_5
+  },
+  {
+    name: "monkey_6",
+    image: monkey_6
+  },
+  {
+    name: "azuki_1",
+    image: azuki_1
+  },
+  {
+    name: "azuki_2",
+    image: azuki_2
+  },
+  {
+    name: "azuki_3",
+    image: azuki_3
+  },
+  {
+    name: "azuki_4",
+    image: azuki_4
+  },
+  {
+    name: "bird_1",
+    image: bird_1
+  },
+  {
+    name: "bird_2",
+    image: bird_2
+  },
+  {
+    name: "bird_3",
+    image: bird_3
+  },
+  {
+    name: "bird_4",
+    image: bird_4
+  },
+  {
+    name: "bird_5",
+    image: bird_5
+  },
+  {
+    name: "bird_6",
+    image: bird_6
+  },
+  {
+    name: "clone_1",
+    image: clone_1
+  },
+  {
+    name: "clone_2",
+    image: clone_2
+  },
+  {
+    name: "clone_3",
+    image: clone_3
+  },
+  {
+    name: "clone_4",
+    image: clone_4
+  },
+  {
+    name: "clone_5",
+    image: clone_5
+  },
+  {
+    name: "clone_6",
+    image: clone_6
+  },
+  {
+    name: "clone_7",
+    image: clone_7
+  },
+  {
+    name: "clone_8",
+    image: clone_8
+  },
+  {
+    name: "clone_9",
+    image: clone_9
+  },
+  {
+    name: "clone_10",
+    image: clone_10
+  },
+  {
+    name: "clone_11",
+    image: clone_11
+  }
+  // {
+  //   name: "person1",
+  //   image: person1
+  // },
+  // {
+  //   name: "person2",
+  //   image: person2
+  // },
+  // {
+  //   name: "person3",
+  //   image: person3
+  // },
+  // {
+  //   name: "maleIcon",
+  //   image: maleIcon
+  // },
+  // {
+  //   name: "guest",
+  //   image: guest
+  // },
+  // {
+  //   name: "home",
+  //   image: home
+  // },
+  // {
+  //   name: "minus",
+  //   image: minus
+  // },
+  // {
+  //   name: "owner",
+  //   image: owner
+  // }
+];
+
+export function map(_arg0, _name) {
+  throw new Error("Function not implemented.");
+}
+export function name(_arg0, _name) {
+  throw new Error("Function not implemented.");
+}
